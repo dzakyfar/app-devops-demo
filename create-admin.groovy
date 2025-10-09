84d3e9b8bcb6ed8e9c8be1572338742e57e42210
dzakyfar <dzakyfr509@gmail.com>
2025-10-09 14:23:02 +0700

    Update fix error

---
+++ create-admin.groovy
@@ -0,0 +1,12 @@
+#!groovy
+import jenkins.model.*
+import hudson.security.*
+
+def instance = Jenkins.getInstance()
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("Dzaky", "Akaza123")
+instance.setSecurityRealm(hudsonRealm)
+def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+instance.setAuthorizationStrategy(strategy)
+instance.save()
+println "--> Admin user created: Dzaky / Akaza123"
